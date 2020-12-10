a07891928f31c22cdce41a32394e31437104ba83
Jani Pasanen <jani.m.pasanen@gmail.com>
2020-12-10 02:24:19 +0100

    Added comments

--- src/main/java/se/janipasanen/knightsofthedinnertable/GameLogic.java
+++ src/main/java/se/janipasanen/knightsofthedinnertable/GameLogic.java
@@ -9,28 +9,33 @@ import java.util.List;
 public class GameLogic {
     void run() {
         String[] input = getInput().split(".\n");
-        HashSet<String> people = new HashSet<>(); // Hold all the people
-        HashMap<String, Integer> happiness = new HashMap<>(); // Hold the happiness links between everyone
+        // Hold all the people
+        HashSet<String> allPeople = new HashSet<>();
+        // Hold the happiness links between everyone
+        HashMap<String, Integer> happiness = new HashMap<>();
         for (String line : input) {
             String[] tokens = line.split(" ");
-            int mult = tokens[2].equals("gain") ? 1 : -1; // Check if it's a gain or loss in happiness
-            people.add(tokens[0]);
-            people.add(tokens[10]);
-            happiness.put(tokens[0] + tokens[10], Integer.parseInt(tokens[3]) * mult); // Create key value pairs of 'name1name2' : weight
+            // Check if it's a gain or loss in happiness
+            int mult = tokens[2].equals("gain") ? 1 : -1;
+            allPeople.add(tokens[0]);
+            allPeople.add(tokens[10]);
+            // Create key value pairs of 'name1name2' : weight
+            happiness.put(tokens[0] + tokens[10], Integer.parseInt(tokens[3]) * mult);
         }
         // True: Part 1 || False: Part 2
-        if (true) {
-            people.remove("me");
-        } else {
-            people.add("me");
-            for (String peep : people) {
-                happiness.put(peep + "me", 0);
-                happiness.put("me" + peep, 0);
+      //  if (true) {
+        //    allPeople.remove("me");
+        //} else {
+            allPeople.add("me");
+            for (String people : allPeople) {
+                happiness.put(people + "me", 0);
+                happiness.put("me" + people, 0);
             }
-        }
+      //  }
         int max = 0;
 
-        for (List<String> perm : Collections2.permutations(people)) { // Get all permumations
+        // Get permumations / The names in different positions in the list to test all possible combinations of positions.
+        for (List<String> perm : Collections2.permutations(allPeople)) {
             int total = 0;
             for (int i = 0; i < perm.size() - 1; i++) {
                 total += happiness.get(perm.get(i) + perm.get(i + 1)) + happiness.get(perm.get(i + 1) + perm.get(i));
